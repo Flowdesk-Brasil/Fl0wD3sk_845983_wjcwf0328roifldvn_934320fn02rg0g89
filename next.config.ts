47c5e85733f3b78d2c82396eb3a4b1ed7e487154
MuriloFlow <255751907+MuriloFlow@users.noreply.github.com>
2026-06-04 14:53:17 -0300

    fix: next.js allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // @ts-ignore
+  allowedDevOrigins: ["192.168.18.136", "localhost"],
 };
 
 export default nextConfig;
